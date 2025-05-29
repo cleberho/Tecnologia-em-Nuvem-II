27e3b019852adb52ba18d640d6ab68d022505212
cleberho <cleberho@hotmail.com>
2025-05-28 21:29:00 -0300

    Update index.php

--- appdel/index.php
+++ appdel/index.php
@@ -1,6 +1,6 @@
 <?php
 // Caminho da pasta onde estão os arquivos
-$arquivo = __DIR__ . '/uploads/formulario.php'; // Altere o nome e caminho conforme necessário
+$arquivo = __DIR__ . '/uploads/appdel/formulario.php'; // Altere o nome e caminho conforme necessário
 
 if (file_exists($arquivo)) {
     unlink($arquivo);
